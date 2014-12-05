2d71fc084acb58952917436956101847afdf3767
Jackeagle <jackeagle102@gmail.com>
2014-12-05 17:02:21 +0530

    Add More Files and Rearrangement Of Tree - Partition Info - Device Info - Moved Some Files - Added Wifi Configs - BoardConfig Additions

--- cm.mk
+++ cm.mk
@@ -14,9 +14,12 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/dior/device_dior.mk)
 
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/dior/dior:4.4.4/KTU84P/4.11.28:user/release-keys PRIVATE_BUILD_DESC="dior-user 4.4.4 KTU84P 4.11.28 release-keys"
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dior
 PRODUCT_NAME := cm_dior
-PRODUCT_BRAND := xiaomi
-PRODUCT_MODEL := dior
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := HM_NOTE_1LTE
+PRODUCT_MANUFACTURER := Xiaomi
